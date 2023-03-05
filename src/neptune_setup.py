8c0b5050c94ce1bb8ed403b87388df247360546f
Global-sourcing-analytics <cezary_bujak@barry-callebaut.com>
2023-03-05 13:54:09 +0100

    Main repository structure #1

---
+++ src/neptune_setup.py
@@ -0,0 +1,16 @@
+import neptune
+
+run = neptune.init_run(
+    project="cezbuj/Cocoa-yield-forecast",
+    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjZWQxYmM4Ni1lMzkyLTQ4NTYtOTQ1YS1mOWZhZTljMzhlOGEifQ==",
+)  # your credentials
+
+params = {"learning_rate": 0.001, "optimizer": "Adam"}
+run["parameters"] = params
+
+for epoch in range(10):
+    run["train/loss"].append(0.9 ** epoch)
+
+run["eval/f1_score"] = 0.66
+
+run.stop()
